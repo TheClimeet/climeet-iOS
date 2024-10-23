9977344be43de68cf1b7de3193e6be48e16b94ec
woogi <azicplus@naver.com>
2024-10-23 21:43:45 +0900

    #2 feat: Naver, Kakao Configuration

--- Climeet-iOS/Climeet-iOS/App/Climeet_iOSApp.swift
+++ Climeet-iOS/Climeet-iOS/App/Climeet_iOSApp.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 import ComposableArchitecture
 import DesignSystem
+import KakaoSDKCommon
+import KakaoSDKAuth
+import NaverThirdPartyLogin
 
 @main
 struct ClimeetiOSApp: App {
     
     init() {
         applyGlobalNavigationTitleAttributes()
+        KakaoSDK.initSDK(appKey: Env.KAKAO_APP_KEY)
+        initNaver()
     }
     
     var body: some Scene {
@@ -25,8 +30,25 @@ struct ClimeetiOSApp: App {
     func applyGlobalNavigationTitleAttributes() {
         UINavigationBar.appearance().titleTextAttributes = [
             .foregroundColor: UIColor(.levelWhite),
-//            .font: UIFont(name: "Pretendard-Bold", size: 18)!
+            //            .font: UIFont(name: "Pretendard-Bold", size: 18)!
             // Font.climeetFontTitle4 해당함
         ]
+    }
+    
+    private func initNaver() {
+        let instance = NaverThirdPartyLoginConnection.getSharedInstance()
+        // 네이버 앱으로 로그인 허용
+        instance?.isNaverAppOauthEnable = true
+        // 브라우저 로그인 허용
+        instance?.isInAppOauthEnable = true
+        
+        // 네이버 로그인 세로모드 고정
+        instance?.setOnlyPortraitSupportInIphone(true)
+        
+        // NaverThirdPartyConstantsForApp.h에 선언한 상수 등록
+        instance?.serviceUrlScheme = "Swift.Climeet.Climeet-iOS"
+        instance?.consumerKey = Env.NAVER_CLIENT_ID
+        instance?.consumerSecret = Env.NAVER_CLIENT_SECRET
+        instance?.appName = "Climeet"
     }
 }
