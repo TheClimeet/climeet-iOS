6b8cf3f182d599894251aff126af7a1c7cf6a3fe
hyeonung Seo <workplayhard1@naver.com>
2024-10-28 16:59:08 +0900

    feat: token Refresher를 생성 시 인자로 받도록 수정구현
    - 목적: 직접적인 뷰 조작은 Kit 외부의 모듈에서 해주기위해

--- NetworkKit/Sources/NetworkKit/Kit/APIInterceptor.swift
+++ NetworkKit/Sources/NetworkKit/Kit/APIInterceptor.swift
@@ -1,3 +1,48 @@
+import Foundation
+import Alamofire
+import NetworkKit
+
 public protocol TokenRefreshable: Sendable {
     func refreshToken() async -> Bool
+}
+
+final class APIInterceptor: RequestInterceptor {
+    
+    private let tokenRefresher: TokenRefreshable
+    
+    init(tokenRefresher: TokenRefreshable) {
+        self.tokenRefresher = tokenRefresher
+    }
+    
+    func adapt(_ urlRequest: URLRequest, for session: Alamofire.Session, completion: @escaping (Result<URLRequest, any Error>) -> Void) {
+        var request = urlRequest
+        guard let token = UserDefaults.standard.value(forKey: "token") as? String else { // Get token
+            completion(.failure(NSError(domain: "TokenError", code: 401, userInfo: ["description": "Token not found in UserDefaults.standards"])))
+            return
+        }
+        
+        request.headers.add(.authorization(bearerToken: token))
+        
+        if let tokenHeader = request.headers.first(where: { $0 == .authorization(bearerToken: token) }) {
+            print("\nadapted; token added to the header field is: \(tokenHeader)\n")
+        }
+        
+        completion(.success(request))
+    }
+    
+    func retry(_ request: Request, for session: Session, dueTo error: any Error, completion: @escaping @Sendable (RetryResult) -> Void) {
+        
+        let retryLimit = 3
+        guard request.retryCount < retryLimit else {
+            completion(.doNotRetry)
+            return
+        }
+        
+        print("\nretried; retry count: \(request.retryCount)\n")
+        
+        Task {
+            let isSuccessRefresh = await self.tokenRefresher.refreshToken()
+            isSuccessRefresh ? completion(.doNotRetry) : completion(.retry)
+        }
+    }
 }
