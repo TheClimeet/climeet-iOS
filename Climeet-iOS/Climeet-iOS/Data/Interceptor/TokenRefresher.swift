a5f3abbd76a7a8c2e8c2bb55f8097f0f4afc4163
hyeonung Seo <workplayhard1@naver.com>
2024-10-29 11:36:40 +0900

    feat: TokenRefresher에 readToken 메서드 추가

--- Climeet-iOS/Climeet-iOS/Data/Interceptor/TokenRefresher.swift
+++ Climeet-iOS/Climeet-iOS/Data/Interceptor/TokenRefresher.swift
@@ -9,6 +9,14 @@ import NetworkKit
 import Foundation
 
 struct TokenRefresher: TokenRefreshable {
+    func readToken() -> String {
+        guard let readToken = KeyChain.shared.refreshToken else {
+            tokenNotFoundAction()
+            return ""
+        }
+        
+        return readToken
+    }
     
     func refreshToken() async -> Bool {
         // TODO: RefreshToken API 호출
@@ -16,13 +24,17 @@ struct TokenRefresher: TokenRefreshable {
         let result = try? await APIClient(tokenRefresher: nil)
             .request(endpoint, decode: UserDTO.RefreshToken.Response.self)
         
-        // TODO: Save RefreshToken to (UserDefaults)
+        // TODO: Save RefreshToken to (KeyChain)
         if let refreshToken = result?.refreshToken {
-            UserDefaults.standard.set(refreshToken, forKey: "token")
-            UserDefaults.standard.synchronize()
+            KeyChain.shared.refreshToken = refreshToken
             return true
         }
         
         return false
+    }
+    
+    // MARK: Private
+    private func tokenNotFoundAction() {
+        // TODO: 토큰이 존재하지않을 때, 로그인화면 이동 처리
     }
 }
