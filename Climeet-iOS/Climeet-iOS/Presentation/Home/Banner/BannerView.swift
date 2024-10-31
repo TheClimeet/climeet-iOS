185be4c20ac79e3e64f1b617394560066e8713a8
ericKwon95 <ericyong95@gmail.com>
2024-10-31 10:29:24 +0900

    feat: 양방향 무한 스크롤 배너 구현
    - 탭 뷰를 갈아끼우는 방식
    - 다만 일정시간마다 바뀌는 애니메이션이 적용 불가하다.

--- Climeet-iOS/Climeet-iOS/Presentation/Home/Banner/BannerView.swift
+++ Climeet-iOS/Climeet-iOS/Presentation/Home/Banner/BannerView.swift
@@ -15,18 +15,42 @@ struct BannerView: View {
     let screenWidth = UIWindow().screen.bounds.width
     
     var body: some View {
-        TabView(selection: $store.selection) {
-            ForEach(store.bannerInfos.indices, id: \.self) { index in
-                KFImage(URL(string: store.bannerInfos[index].bannerImageURL))
-                    .scaledToFill()
-                    .frame(width: screenWidth, height: screenWidth / 2.08)
-                    .clipped()
-                    .tag(index)
+        BidirectionalInfiniteBanner(
+            currentIndex: $store.selection,
+            bannerCount: store.bannerInfos.count,
+            prevURL: {
+                guard !store.bannerInfos.isEmpty else {
+                    return nil
+                }
+                
+                var prevIndex = store.selection - 1
+                if prevIndex == -1 {
+                    prevIndex = store.bannerInfos.count - 1
+                }
+                return URL(string: store.bannerInfos[prevIndex].bannerImageURL)
+            },
+            currentURL: {
+                guard !store.bannerInfos.isEmpty else {
+                    return nil
+                }
+                
+                return URL(string: store.bannerInfos[store.selection].bannerImageURL)
+            },
+            nextURL: {
+                guard !store.bannerInfos.isEmpty else {
+                    return nil
+                }
+                
+                var nextIndex = store.selection + 1
+                if nextIndex == store.bannerInfos.count {
+                    return URL(string: store.bannerInfos[0].bannerImageURL)
+                } else {
+                    return URL(string: store.bannerInfos[nextIndex].bannerImageURL)
+                }
             }
-        }
+        )
         .frame(width: screenWidth, height: screenWidth / 2.08)
         .tabViewStyle(.page(indexDisplayMode: .never))
-        .animation(.bouncy, value: store.selection)
         .overlay {
             VStack {
                 Spacer()
@@ -59,4 +83,61 @@ struct BannerView: View {
         BannerReducer()
     }
     BannerView(store: store)
+}
+
+struct BidirectionalInfiniteBanner: View {
+    
+    @Binding var currentIndex: Int
+    @State private var selection: Int = 0
+    
+    let screenWidth = UIWindow().screen.bounds.width
+    let bannerCount: Int
+    
+    var prevURL: () -> URL?
+    var currentURL: () -> URL?
+    var nextURL: () -> URL?
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            KFImage(prevURL())
+                .scaledToFill()
+                .frame(width: screenWidth, height: screenWidth / 2.08)
+                .clipped()
+                .tag(-1)
+            
+            KFImage(currentURL())
+                .scaledToFill()
+                .frame(width: screenWidth, height: screenWidth / 2.08)
+                .clipped()
+                .tag(0)
+                .onDisappear {
+                    switch selection {
+                    case -1:
+                        if currentIndex - 1 == -1 {
+                            currentIndex = bannerCount - 1
+                        } else {
+                            currentIndex -= 1
+                        }
+                    case 0:
+                        break
+                    case 1:
+                        if currentIndex + 1 == bannerCount {
+                            currentIndex = 0
+                        } else {
+                            currentIndex += 1
+                        }
+                    default:
+                        break
+                    }
+                    selection = 0
+                }
+            
+            KFImage(nextURL())
+                .scaledToFill()
+                .frame(width: screenWidth, height: screenWidth / 2.08)
+                .clipped()
+                .tag(1)
+        }
+        .disabled(selection != 0)
+    }
 }
